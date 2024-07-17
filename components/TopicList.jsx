3f26ed60d578bf770390ff21c0945dff804b4365
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-07-18 02:02:41 +0530

    create navbar

---
+++ components/TopicList.jsx
@@ -0,0 +1,14 @@
+export default function TopicList(){
+    return(
+<>
+<div>
+    <div>
+        <h2>Topic Title</h2>
+        <div>Topic Description</div>
+    </div>
+    <div></div>
+</div>
+</>
+);
+  
+}
