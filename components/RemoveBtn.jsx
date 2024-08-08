11599a016681992512805ab83bd643e54284f612
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-08-09 02:13:04 +0530

    cdelete,update,delete

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,7 +1,37 @@
-import {HiOutlineTrash} from "react-icons/hi"
+'use client';
 
-export default function RemoveBtn() {
-    return <button className="text-red-400">
-        <HiOutlineTrash size={24}/>
-    </button>;
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+
+export default function RemoveBtn({ id }) {
+    const router = useRouter();
+    const removeTopic = async () => {
+        const confirmed = confirm("Are you sure?");
+        if (confirmed) {
+            try {
+                const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+                    method: "DELETE",
+                });
+                if(res.ok){
+                    router.refresh();
+                }
+                
+
+                if (res.ok) {
+                    console.log('Topic deleted successfully');
+                  
+                } else {
+                    throw new Error('Failed to delete the topic');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+    return (
+        <button onClick={removeTopic} className="text-red-400">
+            <HiOutlineTrash size={24} />
+        </button>
+    );
 }
